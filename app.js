31d7dd754012f0f613cc19247a3cbf690b7db916
VanessaLin9 <vanessa7591@gmail.com>
2021-11-29 11:24:20 +0800

    add hbs

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+
 const app = express()
+const PORT = 3000
+
+const db = require('./models')
+
+app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs'}))
 
 app.get('/', (req, res) => {
   res.send('Hello word')
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('app is running on http://localhost:3000')
 })
