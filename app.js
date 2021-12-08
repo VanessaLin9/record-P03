cfbe79aaa8157d603e965f10248826feb14197dd
VanessaLin9 <vanessa7591@gmail.com>
2021-12-09 05:59:13 +0800

    add tw stock model

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const PORT = 3000
 
 const db = require('./models')
+const Twstock = db.Twstock
 
 app.engine('hbs', exphbs({ defaultLayout:'main', extname:'.hbs'}))
 
@@ -12,7 +13,13 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  return Twstock.findAll({
+    raw:true,
+    nest: true,
+    attributes: ['id', 'name']
+  })
+  .then((stock) => res.json({stock:stock}))
+  
 })
 
 app.listen(PORT, () => {
